2edec66190d314d4e00ccd8d52c5a9843d73d850
Matviidev <matvii.shpyruk.dev@gmail.com>
2025-11-06 21:02:03 +0200

    feat(test): add test for create user dto

---
+++ src/users/create-user.dto.spec.ts
@@ -0,0 +1,53 @@
+import { validate } from 'class-validator';
+import { CreateUserDto } from './dto/create-user.dto';
+
+describe('CreateUserDto', () => {
+  let dto = new CreateUserDto();
+
+  beforeEach(() => {
+    dto = new CreateUserDto();
+    dto.email = 'test@test.com';
+    dto.name = 'Piotr';
+    dto.password = '123456A#';
+  });
+
+  it('should validate complete valid data', async () => {
+    const errors = await validate(dto);
+    expect(errors.length).toBe(0);
+  });
+
+  it('should fail on invalid email', async () => {
+    dto.email = 'test';
+    const errors = await validate(dto);
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors[0].property).toBe('email');
+    expect(errors[0].constraints).toHaveProperty('isEmail');
+  });
+
+  const testPassword = async (password: string, message: string) => {
+    dto.password = password;
+    const errors = await validate(dto);
+    const passwordError = errors.find((error) => error.property === 'password');
+    expect(passwordError).not.toBeUndefined();
+    const messages = Object.values(passwordError?.constraints ?? {});
+    expect(messages).toContain(message);
+  };
+
+  it('should fail without 1 uppercase letter', async () => {
+    await testPassword(
+      'abcdef',
+      'Password must contain at least 1 uppercase letter',
+    );
+  });
+
+  it('should fail without at least 1 number', async () => {
+    await testPassword('abdefaA', 'Password must contain at least 1 number');
+  });
+
+  it('should fail without at least 1 special character', async () => {
+    await testPassword(
+      'abdefaA1',
+      'Password must contain at least 1 special character',
+    );
+  });
+});
